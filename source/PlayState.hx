d662cf3da8b1ae3afad9a62c1f277f965bddf803
kralinc <lelinxi@protonmail.com>
2022-02-19 14:04:10 -0600

    Made hello world.

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,16 +1,24 @@
 package;
 
 import flixel.FlxState;
+import flixel.input.gamepad.lists.FlxGamepadButtonList;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
 
 class PlayState extends FlxState
 {
 	var hero:Hero;
+	var helloWorld:FlxText;
 
 	override public function create()
 	{
 		super.create();
 		hero = new Hero(300, 300);
 		add(hero);
+
+		helloWorld = new FlxText(200, 200, 0, "Hello, World!", 30);
+		helloWorld.setFormat("assets/PermanentMarker-Regular.ttf", 45, 0xffaa55);
+		add(helloWorld);
 	}
 
 	override public function update(elapsed:Float)
